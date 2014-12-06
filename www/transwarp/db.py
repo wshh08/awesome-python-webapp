555408283f4575b3ede985283ca4e9164dd24806
wangshaohua <wshh08@126.com>
2014-12-07 00:16:39 +0800

    db module.v2

---
+++ www/transwarp/db.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'wangshaohua'
+
+'''
+DataBase operating module
+'''
+
+import threading
+import logging
+
+
+class Dict(dict):
+    pass
+
+
+class _Engine(object):
+    def __init__(self, connect):
+        self._connect = connect
+
+    def connect(self):
+        return self._connect()
+
+
+def create_engine(user, password, database, host='127.0.0.1', port=3306, **kw):
+    import mysql.connetor
+    global engine
+    if engine is not None:
+        raise DBError('Engine is already initialized.')
+    params = dict(user=user, password=password, database=database, host=host, port=port)
+    defaults = dict(user_unicode=True, charset='utf8', collation='utf8_general-ci', autocommit=False)
+    for k, v in defaults.iteritems():
+        params[k] = kw.pop(k, v)
+    params.update(kw)
+    params['buffered'] = True
+    engine = _Engine(lambda: mysql.connector.connect(**params))
+    # test connection...
+    logging.info('Init mysql engine <%s> ok.' % hex(id(engine)))
+
+engine = None
+
+class _DbCtx(threading.local):
+    def __init__(self):
+        self.connection = None
+        self.transaction = 0
+
+    def is_init(self):
+        return not self.connection is None
+
+    def init(self):
+        self.connection = _LasyConnection()
+        self.transaction = 0
+
+    def cleanup(self):
+        self.connection.cleanup()
+        self.connection = None
+
+    def cursor(self):
+        return self.connection.cursor()
+
+_db_ctx = _DbCtx()

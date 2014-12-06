ce6270c57b49e6022ff2a4ceb8ac8a86a2030529
wangshaohua <wshh08@126.com>
2014-12-07 03:44:40 +0800

    update db,py

--- www/transwarp/db.py
+++ www/transwarp/db.py
@@ -9,10 +9,20 @@ DataBase operating module
 
 import threading
 import logging
+import time
+import functools
 
 
 class Dict(dict):
     pass
+
+
+def _profiling(start, sql=''):
+    t = time.time() - start
+    if t > 0.1:
+        logging.warning('[PROFILING] [DB] %s %s' % (t, sql))
+    else:
+        logging.info('[PROFILING] [DB] %s %s' % (t, sql))
 
 
 class _Engine(object):
@@ -28,8 +38,10 @@ def create_engine(user, password, database, host='127.0.0.1', port=3306, **kw):
     global engine
     if engine is not None:
         raise DBError('Engine is already initialized.')
-    params = dict(user=user, password=password, database=database, host=host, port=port)
-    defaults = dict(user_unicode=True, charset='utf8', collation='utf8_general-ci', autocommit=False)
+    params = dict(user=user, password=password, database=database,
+                  host=host, port=port)
+    defaults = dict(user_unicode=True, charset='utf8',
+                    collation='utf8_general-ci', autocommit=False)
     for k, v in defaults.iteritems():
         params[k] = kw.pop(k, v)
     params.update(kw)
@@ -38,9 +50,78 @@ def create_engine(user, password, database, host='127.0.0.1', port=3306, **kw):
     # test connection...
     logging.info('Init mysql engine <%s> ok.' % hex(id(engine)))
 
-engine = None
+
+def _ConnectionCtx(object):
+    '''
+    _ConnectionCtx object that can open and close connection context.
+    '''
+
+
+
+class _TransactionCtx(object):
+
+    def __enter__(self):
+        global _db_ctx
+        self.should_close_conn = False
+        if not _db_ctx.is_init():
+            # needs open a connection first:
+            _db_ctx.init()
+            self.should_close_conn = True
+        _db_ctx.transaction = _db_ctx.transaction + 1
+        logging.info('begin transaction...' if _db_ctx.transction == 1 else
+                     'join current transcation...')
+        return self
+
+    def __exit__(self, exctype, excvalue, traceback):
+        global _db_ctx
+        _db_ctx.transaction = _db_ctx.transcation - 1
+        try:
+            if _db_ctx.transction == 0:
+                if exctype is None:
+                    self.commit()
+                else:
+                    self.rollback()
+        finally:
+            if self.should_close_conn:
+                _db_ctx.cleanup()
+
+    def commit(self):
+        global _db_ctx
+        logging.info('commit transcation...')
+        try:
+            _db_ctx.connection.commit()
+            logging.info('commit ok.')
+        except:
+            logging.warning('commit failed. try rollback...')
+            _db_ctx.connection.rollback()
+            logging.warning('rollback ok.')
+            raise
+
+    def rollback(self):
+        global _db_ctx
+        logging.warning('rollback trasction...')
+        _db_ctx.connection.rollback()
+        logging.info('rollback ok.')
+
+
+def transaction():
+    return _TransactionCtx()
+
+
+def with_transaction(func):
+    @functools.wraps(func)
+    def _wrapper(*args, **kw):
+        _start = time.time()
+        with _TransactionCtx():
+            return func(*args, **kw)
+        _profiling(_start)
+    return _wrapper
+
 
 class _DbCtx(threading.local):
+    '''
+    Thread local object that holds connection info.
+    '''
     def __init__(self):
         self.connection = None
         self.transaction = 0
@@ -60,3 +141,40 @@ class _DbCtx(threading.local):
         return self.connection.cursor()
 
 _db_ctx = _DbCtx()
+
+engine = None
+
+
+class DBError(Exception):
+    pass
+
+
+class MultiColumnsError(DBError):
+    pass
+
+
+class _LasyConnection(object):
+    def __init__(self):
+        self.connection = None
+
+    def cursor(self):
+        if self.connection is None:
+            connection = engine.connect()
+            logging.info('open connection <%s>...' % hex(id(connection)))
+            self.connection = connection
+        return self.connection.commit()
+
+    def commit(self):
+        self.connection.commit()
+
+    def rollback(self):
+        self.connection.rollback()
+
+    def cleanup(self):
+        if self.connection:
+            connection = self.connection
+            self.connection = None
+            logging.info('Close connection <%s>...' % hex(id(connection)))
+            connection.close()
+
+

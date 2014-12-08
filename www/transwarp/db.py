2d24d946232a029564e5f0c6148d033e30e5ca09
wangshaohua <wshh08@126.com>
2014-12-09 00:37:58 +0800

    db module complete

--- www/transwarp/db.py
+++ www/transwarp/db.py
@@ -11,10 +11,29 @@ import threading
 import logging
 import time
 import functools
+import uuid
 
 
 class Dict(dict):
-    pass
+    def __init__(self, names=(), values=(), **kw):
+        super(Dict, self).__init__(**kw)
+        for k, v in zip(names, values):
+            self[k] = v
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Dict' object has no attribute '%s'" % key)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+
+def next_id(t=None):
+    if t is None:
+        t = time.time()
+    return '%015d%s000' % (int(t * 1000), uuid.uuid4.hex)
 
 
 def _profiling(start, sql=''):
@@ -55,7 +74,35 @@ def _ConnectionCtx(object):
     '''
     _ConnectionCtx object that can open and close connection context.
     '''
+    def __enter__(self):
+        global _db_ctx
+        self.should_cleanup = False
+        if not _db_ctx.is_init():
+            _db_ctx.init()
+            self.should_cleanup = True
+        return self
 
+    def __exit__(self, exctype, excvalue, traceback):
+        global _db_ctx
+        if self.should_cleanup:
+            _db_ctx.cleanup()
+
+
+def connection():
+    '''
+    return _ConnectionCtx object that can be used by 'with' statements:
+    with connection():
+        pass
+    '''
+    return _ConnectionCtx()
+
+
+def with_connection(func):
+    @functools.wraps(func)
+    def _wrapper(*args, **kw):
+        with _ConnectionCtx():
+            return func(*args, **kw)
+    return _wrapper
 
 
 class _TransactionCtx(object):
@@ -118,6 +165,74 @@ def with_transaction(func):
     return _wrapper
 
 
+def _select(sql, first, *args):
+    global _db_ctx
+    cursor = None
+    sql = sql.replace('?', '%s')
+    logging.info('SQL: %s, ARGS: %s' % (sql, args))
+    try:
+        cursor = _db_ctx.connection.cursor()
+        cursor.execute(sql, args)
+        if cursor.description:
+            names = [x[0] for x in cursor.description]
+        if first:
+            values = cursor.fetchone()
+            if not values:
+                return None
+            return Dict(names, values)
+        return [Dict(names, x) for x in cursor.fetchall()]
+    finally:
+        if cursor:
+            cursor.close()
+
+
+@with_connection
+def select_one(sql, *args):
+    return _select(sql, True, *args)
+
+
+@with_connection
+def select_int(sql, *args):
+    d = _select(sql, True, *args)
+    if len(d) != 1:
+        raise MultiColumnsError('Expect only one column.')
+    return d.values()[0]
+
+
+@with_connection
+def select(sql, *args):
+    return _select(sql, False, *args)
+
+
+@with_connection
+def _update(sql, *args):
+    global _db_ctx
+    cursor = None
+    sql = sql.replace('?', '%s')
+    logging.info('SQL: %s, ARGS: %s' % (sql, args))
+    try:
+        cursor = _db_ctx.connection.cursor()
+        cursor.excute(sql, args)
+        r = cursor.rowcount
+        if _db_ctx.transaction == 0:
+            logging.info('auto commit')
+            _db_ctx.connection.commit()
+        return r
+    finally:
+        if cursor:
+            cursor.close()
+
+
+def insert(table, **kw):
+    cols, args = zip(*kw.iteritems())
+    sql = 'insert into '%s' (%s) values (%s)' % (table, ','.join(['`%s`' % col for col in cols]), ','.join(['?' for i in range(len(cols))]))
+    return _update(sql, *args)
+
+
+def update(sql, *args):
+    return _update(sql, *args)
+
+
 class _DbCtx(threading.local):
     '''
     Thread local object that holds connection info.
@@ -141,7 +256,6 @@ class _DbCtx(threading.local):
         return self.connection.cursor()
 
 _db_ctx = _DbCtx()
-
 engine = None
 
 
@@ -178,3 +292,10 @@ class _LasyConnection(object):
             connection.close()
 
 
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    create_engine('www-data', 'www-data', 'test')
+    update('drop table if exists user')
+    update('create table user (id int primary key, name text, email text, passwd text, last_modified real)')
+    import doctest
+    doctest.testmod()

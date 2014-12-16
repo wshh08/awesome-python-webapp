101bf3be8eb5db8e7942a87f4ad72efcda6519c0
wangshaohua <wshh08@126.com>
2014-12-17 02:22:21 +0800

    Update web.py

--- www/transwarp/web.py
+++ www/transwarp/web.py
@@ -12,6 +12,12 @@ from db import Dict
 import datetime
 import re
 import urllib
+import os
+import mimetypes
+import cgi
+import logging
+import functools
+import _debug
 
 
 ctx = threading.local()
@@ -373,62 +379,343 @@ class Route(object):
     __repr__ = __str__
 
 
-class Request(object):
-    def get(self, key, default=None):
-        pass
+def _static_file_generator(fpath):
+    BLOCK_SIZE = 8192
+    with open(fpath, 'rb') as f:
+        block = f.read(BLOCK_SIZE)
+        while block:
+            yield block
+            block = f.read(BLOCK_SIZE)
 
-    def input(self):
-        pass
+
+def StaticFileRoute(object):
+    def __init__(self):
+        self.method = 'GET'
+        self.is_static = False
+        self.route = re.compile('^/static/(.+)$')
+
+    def match(self, url):
+        if url.startswith('/static/'):
+            return (url[1:], )
+        return None
+
+    def __call__(self, *args):
+        fpath = os.path.join(ctx.application.document_root, args[0])
+        if not os.path.isfile(fpath):
+            raise notfound()
+        fext = os.path.splitext(fpath)[1]
+        ctx.response.content_type = mimetypes.types_map.get(fext.lower(), 'application/octet-stream')
+        return _static_file_generator(fpath)
+
+
+def favicon_handler():
+    return _static_file_generator('/favicon.ico')   # static_file_handler('/favico.ico')????
+
+
+class MultipartFile(object):
+    def __init__(self, storage):
+        self.filename = _to_unicode(storage.filename)
+        self.file = storage.file
+
+
+class Request(object):
+    def __init__(self, environ):
+        self._environ = environ
+
+    def _parse_input(self):
+        def _convert(item):
+            if isinstance(item, list):
+                return [_to_unicode(i.value) for i in item]
+            if item.filename:
+                return MultipartFile(item)
+            return _to_unicode(item. value)
+        fs = cgi.FieldStorage(fp=self._environ['wsgi.input'], environ=self._environ, keep_blank_values=True)
+        inputs = dict()
+        for key in fs:
+            inputs[key] = _convert(fs[key])
+        return inputs
+
+    def _get_raw_input(self):
+        if not hasattr(self, '_raw_input'):
+            self._raw_input = self._parse_input()
+        return self._raw_input
+
+    def __getitem__(self, key):
+        r = self._get_raw_input()[key]
+        if isinstance(r, list):
+            return r[0]
+        return r
+
+    def get(self, key, default=None):
+        r = self._get_raw_input().get(key, default)
+        if isinstance(r, list):
+            return r[0]
+        return r
+
+    def gets(self, key):
+        r = self._get_raw_input()[key]
+        if isinstance(r, list):
+            return r[:]
+        return [r]
+
+    def input(self, **kw):
+        copy = Dict(**kw)
+        raw = self._get_raw_input()
+        for k, v in raw.iteritems():
+            copy[k] = v[0] if isinstance(v, list) else v
+        return copy
+
+    def get_body(self):
+        fp = self._environ['wsgi.input']
+        return fp.read()
+
+    @property
+    def remote_addr(self):
+        return self._environ.get('REMOTE_ADDR', '0.0.0.0.0')
+
+    @property
+    def document_root(self):
+        return self._environ.get('DOCUMENT_ROOT', '')
+
+    @property
+    def query_string(self):
+        return self._environ.get('QUERY_STRING', '')
+
+    @property
+    def environ(self):
+        return self._eviron
+
+    @property
+    def request_method(self):
+        return self._environ['REQUEST_METHOD']
 
     @property
     def path_info(self):
-        pass
+        return urllib.unquote(self._environ.get('PATH_INFO', ''))
+
+    @property
+    def host(self):
+        return self._environ.get('HTTP_HOST', '')
+
+    def _get_headers(self):
+        if not hasattr(self, '_headers'):
+            hdrs = {}
+            for k, v in self._environ.iteritems():
+                if k.startswith('HTTP_'):
+                    # convert 'HTTP_ACCEPT_ENCODING' to 'ACCEPT-ENCODING'
+                    hdrs[k[5:].replace('_', '-').upper()] = v.decode('utf-8')
+            self._headers = hdrs
+        return self._headers
 
     @property
     def headers(self):
-        pass
+        return dict(**self._get_headers())
+
+    def header(self, header, default=None):
+        return self._get_headers().get(header.upper(), default)
+
+    def _get_cookies(self):
+        if not hasattr(self, '_cookies'):
+            cookies = {}
+            cookie_str = self._environ.get('HTTP_COOKIE')
+            if cookie_str:
+                for c in cookie_str.split(';'):
+                    pos = c.find('=')
+                    if pos > 0:
+                        cookies[c[:pos].strip()] = _unquote(c[pos+1])
+            self._cookies = cookies
+        return self._cookies
+
+    @property
+    def cookies(self):
+        return Dict(**self._get_cookies())
 
     def cookie(self, name, default=None):
-        pass
+        return self._get_cookies().get(name, default)
+
+UTC_0 = UTC('+00:00')
 
 
 class Response(object):
-    def set_header(self, key, value):
-        pass
+    def __init__(self):
+        self._status = '200 OK'
+        self._headers = {'CONTENT-TYPE': 'text/html; charset=utf-8'}
 
-    def set_cookie(self, name, value, max_age=None, expires=None, path='/'):
-        pass
+    @property
+    def headers(self):
+        L = [(_RESPONSE_HEADER_DICT.get(k, k), v) for k, v in self._headers.iteritems()]
+        if hasattr(self, '_cookies'):
+            for v in self._cookies.itervalues():
+                L.append(('Set-Cookie', v))
+        L.append(_HEADER_X_POWERED_BY)
+        return L
+
+    def header(self, name):
+        key = name.upper()
+        if key not in _RESPONSE_HEADER_DICT:
+            key = name
+        return self._headers.get(key)
+
+    def unset_header(self, name):
+        key = name.upper()
+        if key not in _RESPONSE_HEADER_DICT:
+            key = name
+        if key in self._headers:
+            del self._headers[key]
+
+    def set_header(self, name, value):
+        key = name.upper()
+        if key not in _RESPONSE_HEADER_DICT:
+            key = name
+        self._headers[key] = _to_str(value)
+
+    @property
+    def content_type(self):
+        return self.header('CONTENT-TYPE')
+
+    @content_type.setter
+    def content_type(self, value):
+        if value:
+            self.set_header('CONTENT-TYPE', value)
+        else:
+            self.unset_header('CONTENT-TYPE')
+
+    @property
+    def content_length(self):
+        return self.header('CONTENT-LENGTH')
+
+    @content_length.setter
+    def content_length(self, value):
+        self.set_header('CONTENT-LENGTH', str(value))
+
+    def delete_cookie(self, name):
+        self.set_cookie(name, '__deleted__', expires=0)
+
+    def set_cookie(self, name, value, max_age=None, expires=None, path='/', domain=None, secure=False, http_only=True):
+        if not hasattr(self, '_cookies'):
+            self._cookies = {}
+        L = ['%s=%s' % (_quote(name), _quote(value))]
+        if expires is not None:
+            if isinstance(expires, (float, int, long)):
+                L.append('Expires=%s' % datetime.datetime.fromtimestamp(expires, UTC_0).strftime('%a, %d-%b-%Y %H:%M:%S GMT'))
+            if isinstance(expires, (datetime.date, datetime.datetime)):
+                L.append('Expires=%s' % expires.astimezone(UTC_0).strftime('%a, %d-%b-%Y %H:%M:%S GMT'))
+        elif isinstance(max_age, (int, long)):
+            L.append('Max-Age=%d' % max_age)
+        L.append('Path=%s' % path)
+        if domain:
+            L.append('Domain=%s' % domain)
+        if secure:
+            L.append('Secure')
+        if http_only:
+            L.append('HttpOnly')
+        self._cookies[name] = ';'.join(L)
+
+    def unset_cookie(self, name):
+        if hasattr(self, '_cookies'):
+            if name in self._cookies:
+                del self._cookies[name]
+
+    @property
+    def status_code(self):
+        return int(self._status[:3])
 
     @property
     def status(self):
-        pass
+        return self._status
 
     @status.setter
     def status(self, value):
-        pass
+        if isinstance(value, (int, long)):
+            if value >= 100 and value <= 999:
+                st = _RESPONSE_STATUSES.get(value, '')
+                if st:
+                    self._status = '%d %s' % (value, st)
+                else:
+                    self._status = str(value)
+            else:
+                raise ValueError('Bad response code: %d' % value)
+        elif isinstance(value, basestring):
+            if isinstance(value, unicode):
+                value = value.encode('utf-8')
+            if _RE_RESPONSE_STATUS.match(value):
+                self._status = value
+            else:
+                raise ValueError('Bad response code: %s' % value)
+        else:
+            raise TypeError('Bad type of response code.')
 
 
-    pass
-
-
-def interceptor(pattern):
-    pass
+class Template(object):
+    def __init__(self, template_name, **kw):
+        self.template_name = template_name
+        self.model = dict(**kw)
 
 
 # 定义模板引擎
 class TemplateEngine(object):
     def __call__(self, path, model):
-        pass
+        return '<!-- override this method to render template -->'
 
 
 # 缺省使用Jinja2引擎:
 class Jinja2TemplateEngine(TemplateEngine):
     def __init__(self, templ_dir, **kw):
         from jinja2 import Environment, FileSystemLoader
+        if 'autoescape' not in kw:
+            kw['autoescape'] = True
         self._env = Environment(loader=FileSystemLoader(templ_dir), **kw)
+
+    def add_filter(self, name, fn_filter):
+        self._env.filters[name] = fn_filter
 
     def __call__(self, path, model):
         return self._env.get_template(path).render(**model).encode('utf-8')
+
+
+def _default_error_handler(e, start_response, is_debug):
+    if isinstance(e, HttpError):
+        logging.info('HttpError: %s' % e.status)
+        headers = e.headers[:]
+        headers.append(('Content-Type', 'text/html'))
+        start_response(e.status, headers)
+        return ('<html><body><h1>%s</h1></body></html>' % e.status)
+    logging.exception('Exception:')
+    start_response('500 Internal Server Error', [('Content-Type', 'text/html'), _HEADER_X_POWERD_BY])
+    if is_debug:
+        return _debug()
+    return ('<html><body><h1>500 Internal Server Error</h1><h3>%s</h3></body></html>' % str(e))
+
+
+def view(path):
+    def _decorator(func):
+        @functools.wraps(func)
+        def _wrapper(*args, **kw):
+            r = func(*args, **kw)
+            if isinstance(r, dict):
+                logging.info('return Template')
+                return Template(path, **r)
+            raise ValueError('Expect return a dict when using @view() decorator.')
+        return _wrapper
+    return _decorator
+
+
+_RE_INTERCEPTOR_STARTS_WITH = re.compile(r'^([^\*\?]+)\*?$')
+_RE_INTERCEPTOR_ENDS_WITH = re.compile(r'^\*([^\*\?]+)$')
+
+
+def _build_pattern_fn(pattern):
+    m = _RE_INTERCEPTOR_STARTS_WITH.match(pattern)
+    if m:
+        return lambda p: p.startswith(m.group(1))
+    m = _RE_INTERCEPTOR_ENDS_WITH.match(pattern)
+    if m:
+        return lambda p: p.endswith(m.group(1))
+    raise ValueError('Invalid pattern definition in interceptor.')
+
+
+def interceptor(pattern):
+    pass
 
 
 class WSGIApplication(object):
